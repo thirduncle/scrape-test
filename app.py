e1a39d7c4279db201d0f3284568223ded6400f6e
mkir <mkir@debpc.home>
2021-06-12 11:05:13 +0300

    View results with Flask

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import models
+from models import ScrapeWell_Les_1
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    sale_listings = ScrapeWell_Les_1.go()
+    return sale_listings
